7e2ade3ea088b5f129d35544ac725bd9d9cfd5a9
VictorPko73 <152915385+VictorPko73@users.noreply.github.com>
2025-09-01 19:34:18 +0000

    Equipo pasta y pollo

--- exercises/14-Create-A-For-Loop/app.py
+++ exercises/14-Create-A-For-Loop/app.py
@@ -1,5 +1,6 @@
 def standards_maker():
-    # ✅↓ Write your code here ↓✅
-
-
+    for i in range (0,300):
+        print ("Haré preguntas si estoy atascado")
 # ✅↓ remember to call the function outside (here) ↓✅
+
+standards_maker()
