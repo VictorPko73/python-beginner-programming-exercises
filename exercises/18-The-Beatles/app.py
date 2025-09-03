91808a7c18ab9e21791c60de15261a24e3a0fb2b
VictorPko73 <152915385+VictorPko73@users.noreply.github.com>
2025-09-03 16:35:17 +0000

    pasta y pollo

--- exercises/18-The-Beatles/app.py
+++ exercises/18-The-Beatles/app.py
@@ -1,2 +1,13 @@
 # ✅↓ Write your code here ↓✅
+def sing():
+    letra = ""
+    for i in range (11):
+        if i == 4:
+            letra += "there will be an answer,\n"
+        elif i == 10:
+            letra += "whisper words of wisdom, let it be"
+        else:
+            letra += "let it be,\n"
+    return letra
 
+sing()
